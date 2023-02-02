608f9fe5dfc1a4f928ac709137f494160e6e63eb
AlessandroDiNardo <alexdna003@gmail.com>
2023-02-02 11:08:16 +0100

    create migrations for train, add it in database

---
+++ database/migrations/2023_02_02_100528_create_train_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train', function (Blueprint $table) {
+            $table->id();
+            $table ->string('train_code', 10) -> unique();
+            $table ->string('departing_from', 20);
+            $table ->string('departing_to' , 20);
+            $table ->smallInteger('binary')->unsigned();
+            $table ->dateTime('departing_date');
+            $table ->smallInteger('wagons');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train');
+    }
+};
